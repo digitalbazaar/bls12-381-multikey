10fac97490fb13dfa15c2bfb7924b4fdc3efb8ea
Dave Longley <dlongley@digitalbazaar.com>
2024-01-09 17:33:27 -0500

    Add `Verify` tests.

--- test/bbs.spec.js
+++ test/bbs.spec.js
@@ -5,23 +5,27 @@ import * as chai from 'chai';
 import {
   create_generators, messages_to_scalars, mocked_calculate_random_scalars
 } from '../lib/bbs/util.js';
-import {ProofGen, Sign} from '../lib/bbs/interface.js';
+import {ProofGen, Sign, Verify} from '../lib/bbs/interface.js';
 import {CIPHERSUITES_TEST_VECTORS} from './bbs-test-vectors.js';
 chai.should();
 
 const OPERATIONS = {
   create_generators, messages_to_scalars, mocked_calculate_random_scalars,
-  Sign, ProofGen
+  Sign, ProofGen, Verify
 };
 
 describe.only('BBS test vectors', () => {
-  for(const tv of CIPHERSUITES_TEST_VECTORS) {
+  const only = CIPHERSUITES_TEST_VECTORS.filter(tv => {
+    return tv.fixtures.some(({only}) => only);
+  });
+  const testCiphersuites = only.length > 0 ? only : CIPHERSUITES_TEST_VECTORS;
+  for(const tv of testCiphersuites) {
     const {ciphersuite, fixtures} = tv;
     describe(ciphersuite.name, () => {
       const only = fixtures.filter(({only}) => only);
       const tests = only.length > 0 ? only : fixtures;
       for(const {name, operation, parameters, output} of tests) {
-        it(name, async () => {
+        it(operation + ' - ' + name, async () => {
           const op = OPERATIONS[operation];
           if(!op) {
             throw new Error(`Unknown operation "${operation}".`);
